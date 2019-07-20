bb13b3d586cbd2d780a6dd80be7eb3eb9cf25bb3
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-19 21:53:05 -0700

    Add React Router for Search/Saved page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello, World!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Search} />
+                    <Route exact path="/search" component={Search} />
+                    <Route exact path="/saved" component={Saved} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
