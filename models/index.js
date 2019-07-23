b7793d43ad42ec6addb9bd88961b83f66d3e6bc9
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-22 20:38:02 -0700

    Complete GET, POST, and DELETE API routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 // Exporting an object containing all the models
 module.exports = {
-    Article: require("./Book")
+    Book: require("./Book")
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,32 @@ const db = require("../models");
 
 // Export API routes
 module.exports = app => {
+    // Getting all the books from the database
     app.get("/api/books", (req, res) => {
-
+        db.Book.find({})
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
     });
 
+    // Adding a book to the database
     app.post("/api/books", (req, res) => {
-
+        db.Book.create({
+            title: req.body.title,
+            authors: req.body.authors,
+            description: req.body.description,
+            image: req.body.image,
+            link: req.body.link
+        })
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
     });
 
+    // Delete a specified book from the database
     app.delete("/api/books/:id", (req, res) => {
-
+        db.Book.deleteOne({
+            _id: req.params.id
+        })
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
     });
 };
